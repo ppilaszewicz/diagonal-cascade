fa7202d9a89dc04fb69d8802a51f6dc8348e09ac
ppilaszewicz <71039101+ppilaszewicz@users.noreply.github.com>
2021-03-08 15:30:38 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+input.onButtonPressed(Button.A, function () {
+    max = 4
+    y = 0
+})
+input.onButtonPressed(Button.AB, function () {
+    led.toggle(0, 0)
+    basic.pause(100)
+    led.toggle(0, 1)
+    led.toggle(1, 0)
+    basic.pause(100)
+    led.toggle(2, 0)
+    led.toggle(1, 1)
+    led.toggle(0, 2)
+    basic.pause(100)
+    led.toggle(3, 0)
+    led.toggle(2, 1)
+    led.toggle(1, 2)
+    led.toggle(0, 3)
+    basic.pause(100)
+    led.toggle(4, 0)
+    led.toggle(3, 1)
+    led.toggle(2, 2)
+    led.toggle(1, 3)
+    led.toggle(0, 4)
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index <= 6; index++) {
+        led.plot(0, 0)
+        x = 0
+        y = index + 1
+        while (y >= 0 && x <= 4) {
+            led.plot(x, y)
+            y += -1
+            x += 1
+            basic.pause(10)
+        }
+    }
+})
+let x = 0
+let y = 0
+let max = 0
+let index = 0
+basic.forever(function () {
+	
+})
