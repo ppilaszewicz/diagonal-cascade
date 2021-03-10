8c366ad4877541593be97f707f3dc51d89dad773
ppilaszewicz <71039101+ppilaszewicz@users.noreply.github.com>
2021-03-10 09:17:05 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -26,11 +26,6 @@ input.onButtonPressed(Button.AB, function () {
     led.toggle(1, 2)
     led.toggle(0, 3)
     basic.pause(100)
-    led.toggle(4, 0)
-    led.toggle(3, 1)
-    led.toggle(2, 2)
-    led.toggle(1, 3)
-    led.toggle(0, 4)
 })
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index <= 7; index++) {
@@ -44,6 +39,27 @@ input.onButtonPressed(Button.B, function () {
             basic.pause(10)
         }
     }
+})
+input.onGesture(Gesture.Shake, function () {
+    led.toggle(4, 0)
+    led.toggle(3, 1)
+    led.toggle(2, 2)
+    led.toggle(1, 3)
+    led.toggle(0, 4)
+    basic.pause(100)
+    led.toggle(4, 1)
+    led.toggle(3, 2)
+    led.toggle(2, 3)
+    led.toggle(1, 4)
+    basic.pause(100)
+    led.toggle(4, 2)
+    led.toggle(3, 3)
+    led.toggle(2, 4)
+    basic.pause(100)
+    led.toggle(4, 3)
+    led.toggle(3, 4)
+    basic.pause(100)
+    led.toggle(4, 4)
 })
 let y = 0
 let x = 0
